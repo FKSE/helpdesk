30aa02d300ee60606d5408a9d210c4fec707e255
Frido Koch <info@fridokoch.de>
2015-05-31 16:38:29 +0200

    Tes

--- helpdesk.go
+++ helpdesk.go
@@ -1,12 +1,15 @@
 package helpdesk
 
 type ConfigDatabase struct {
-	Driver string
-	
+	Driver   string
+	Host     string
+	Name     string
+	User     string
+	Password string
+	Charset  string
 }
 
 type Configuration struct {
-
 }
 
 func Run()
